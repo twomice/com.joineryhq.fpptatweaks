3d085a9ecf6015f1b3986c9d7690c2d0c22c0283
Lucky <lucky091588@gmail.com>
2020-12-30 09:47:22 +0800

    Remove CRM_Profile_Form_Edit.js and add css

--- fpptatweaks.php
+++ fpptatweaks.php
@@ -331,13 +331,7 @@ function fpptatweaks_civicrm_buildForm($formName, &$form) {
         $bhfe[] = 'org_relationship';
         $form->assign('beginHookFormElements', $bhfe);
 
-        // TODO: use css to make table styling:        
-//    background: #eee;
-//    width: 100%;
-
-        CRM_Core_Resources::singleton()->addScriptFile('com.joineryhq.fpptatweaks', 'js/CRM_Profile_Form_Edit.js');
-        
-        
+        CRM_Core_Resources::singleton()->addStyleFile('com.joineryhq.fpptatweaks', 'css/CRM_Profile_Form_Edit.css', 100, 'page-header');
       }
     }
   }
