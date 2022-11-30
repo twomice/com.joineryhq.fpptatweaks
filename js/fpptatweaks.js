3744398a85432616406b31cb169c7fecdaa01553
Allen Shaw <allen@JoineryHQ.com>
2022-11-30 13:41:04 -0600

    User Dashboard: Remove clickable link to event in Events section.
    Reference: https://joinery.freshdesk.com/a/tickets/875

--- js/fpptatweaks.js
+++ js/fpptatweaks.js
@@ -1,9 +1,22 @@
 CRM.$(function($) {
   CRM.$( document ).ajaxStop(function() {
-    var requestEndRelationshipUrl;
-    // Tweak contents of the "Your Contacts / Organizations" secion.
-    CRM.$('.crm-contact-relationship-user tbody > tr').each(function(){
+    // Tweak contents of the "Your Event(s)" secion.
+    CRM.$('.crm-dashboard-civievent .view-content tbody > tr').each(function(){
       // Remove link to relationship (in "Relationship" column")
+      var eventTitle = $('> td:first-child a', this).text();
+      if($('> td:first-child a', this).length) {
+        $('> td:first-child', this).append('<span>' + eventTitle + '</span>').find('a:first-child').remove();
+      }
+    });
+    // Also remove the description in this section, which tends to imply that the user
+    // can "Click on the event name for more information."
+    CRM.$('.crm-dashboard-civievent .view-content .description').hide();
+
+    // Tweak contents of the "Your Contacts / Organizations" secion.
+    var requestEndRelationshipUrl;
+    CRM.$('.crm-contact-relationship-user tbody > tr').each(function(){
+      // Remove link to relationship (in "Relationship" column"), and
+      // also remove link to related contact (in second column).
       var relText = $('> td:first-child a', this).text(),
           relOrgIcon = $('> td:nth-child(2) a:first-child', this).html(),
           relOrgText = $('> td:nth-child(2) a:nth-child(2)', this).text();
