8e115a1d014df0fafa6e124c5939347ab371d50d
Allen Shaw <allen@JoineryHQ.com>
2022-11-30 13:30:59 -0600

    NFC: add code comments in fpptatweaks.js.

--- js/fpptatweaks.js
+++ js/fpptatweaks.js
@@ -1,17 +1,18 @@
 CRM.$(function($) {
   CRM.$( document ).ajaxStop(function() {
     var requestEndRelationshipUrl;
+    // Tweak contents of the "Your Contacts / Organizations" secion.
     CRM.$('.crm-contact-relationship-user tbody > tr').each(function(){
+      // Remove link to relationship (in "Relationship" column")
       var relText = $('> td:first-child a', this).text(),
           relOrgIcon = $('> td:nth-child(2) a:first-child', this).html(),
           relOrgText = $('> td:nth-child(2) a:nth-child(2)', this).text();
-
       if($('> td:first-child a', this).length) {
         $('> td:first-child', this).append('<span>' + relText + '</span>').find('a:first-child').remove();
         $('> td:nth-child(2)', this).append(relOrgIcon + '<span>' + relOrgText + '</span>').find('a').remove();
       }
 
-      // Insert 'remove' link
+      // Insert 'remove relationship' link
       requestEndRelationshipUrl = CRM.url('civicrm/fppta/reqendrship', {rid: $(this).attr('data-id')});
       $('> td:last-child a[title="Disable Relationship"]', this).after('<a href="' + requestEndRelationshipUrl + '">Remove</a>');
 
